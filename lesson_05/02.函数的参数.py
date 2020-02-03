c5178771e792a37664ce749069cb6454d91590e7
e_kxc <e_kxc@hotmail.com>
2020-02-03 22:59:00 +0800

    123

--- lesson_05/02.函数的参数.py
+++ lesson_05/02.函数的参数.py
@@ -1,21 +1,28 @@
+# encoding:utf-8
+
 #定义
 def fn(a):
 	print('a = ', a)	
 
-def fn1(a, b, c):
+def fn1(name, a, b, c):
+	print('---例子---', name)
 	print('a = ', a)	
 	print('b = ', b)	
 	print('c = ', c)	
 
+#----------
+
+
 fn(1)
 
 # 关键字参数
-fn1(a=10, c=20, b=1)
+fn1(name='关键字参数',a=10, c=20, b=1)
 
 # 关键字参数与位置参数混合
+fn1('关键字参数与位置参数混合', 10, c='a', b=1)
 
 # 实参类型可以为任意类型
-fn1('h', 2, 'abc')
+fn1('实参类型可以为任意类型', 'h', 2, 'abc')
 
 # 实参可以传递函数
 fn(fn1)
